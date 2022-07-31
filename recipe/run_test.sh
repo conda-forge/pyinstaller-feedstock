9720e780c0961386f6c90ae53b60d439fd35277d
Nehal J Wani <nehaljw.kkd1@gmail.com>
2022-07-31 12:49:32 -0400

    Ensure codesign is picked from /usr/bin

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,7 +1,9 @@
 # Remove the test directory which import tkinter which requires X11
 rm -rf $SP_DIR/../test
 
-pyinstaller -n hello hello.py
+# Pick codesign from /usr/bin instead of /Users/runner/miniforge3/bin
+# xref: https://github.com/thefloweringash/sigtool/issues/12
+PATH=/usr/bin:$PATH pyinstaller -n hello hello.py
 
 ls -lh dist/hello
 dist/hello/./hello
